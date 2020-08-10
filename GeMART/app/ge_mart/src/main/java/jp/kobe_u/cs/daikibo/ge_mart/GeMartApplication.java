68198d5985e249d734a630c2b913c92cfdbee5f6
murotani <mu.1ok.san@gmail.com>
2020-08-10 09:27:27 +0900

    spring_initializr

---
+++ GeMART/app/ge_mart/src/main/java/jp/kobe_u/cs/daikibo/ge_mart/GeMartApplication.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.ge_mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeMartApplication.class, args);
+	}
+
+}
